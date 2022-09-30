2e69328ded4455d3b6e4bb96fbb476be3a8645e4
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-01 00:05:35 +0300

    State edited. (Migrations)

--- backend/database/migrations/2022_09_14_200718_create_states_table.php
+++ backend/database/migrations/2022_09_14_200718_create_states_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('states', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('name');
             $table->foreignId('city_id');
             $table->timestamps();
         });
