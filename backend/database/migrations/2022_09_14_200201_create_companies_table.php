3ab6c368fb2f455c7ec39915d7a42c5e0d31cc4b
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-01 00:14:10 +0300

    Company edited. (Model, Migrations)

--- backend/database/migrations/2022_09_14_200201_create_companies_table.php
+++ backend/database/migrations/2022_09_14_200201_create_companies_table.php
@@ -18,16 +18,16 @@ return new class extends Migration
             $table->string('name');
             $table->string('subdomain');
             $table->boolean('status')->default(1);
-            $table->string('tax_id',11);
+            $table->foreignId('tax_id',11);
             $table->string('email');
             $table->string('web_url')->nullable();
             $table->string('phone');
+            $table->string('logo')->default('/companies/default.png');
             $table->foreignId('country_id');
             $table->foreignId('city_id');
             $table->foreignId('state_id');
             $table->string('address',600);
             $table->string('zip_code');
-            $table->string('logo')->default('/companies/default.png');
             $table->foreignId('payment_plan_id');
             $table->timestamps();
             $table->softDeletes();
