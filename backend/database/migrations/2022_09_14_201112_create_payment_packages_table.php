79a90bc6e8b6aaa63c27fb957c199039e6ee94ab
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-01 00:09:16 +0300

    Payment Package edited. (Model, Migrations)

--- backend/database/migrations/2022_09_14_201112_create_payment_packages_table.php
+++ backend/database/migrations/2022_09_14_201112_create_payment_packages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('payment_packages', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('name');
             $table->string('description',400);
             $table->decimal('price');
             $table->foreignId('plan_id');
