acb858fe784249d9836decaa0f01a20e862cc5b8
Ali KARABAY <alikarabay77@gmail.com>
2022-10-02 02:53:26 +0300

    Lesson model and migration added.

---
+++ backend/app/Models/Lesson.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Lesson extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'content',
+        'category_id',
+        'language_id',
+    ];
+}
---
+++ backend/database/migrations/2022_10_01_234950_create_lessons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->uuid()->index();
+            $table->string('name');
+            $table->longText('content');
+            $table->uuid('category_id')->index();
+            $table->uuid('language_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
