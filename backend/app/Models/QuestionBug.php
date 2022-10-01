2eebfc699a8a7e90487960832c02c00fab4a153d
Ali KARABAY <alikarabay77@gmail.com>
2022-10-01 22:48:12 +0300

    QuestionBug model and migration added.

---
+++ backend/app/Models/QuestionBug.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class QuestionBug extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'description',
+        'question_id',
+    ];
+}
---
+++ backend/database/migrations/2022_10_01_193533_create_question_bugs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_bugs', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('description');
+            $table->uuid('question_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_bugs');
+    }
+};
