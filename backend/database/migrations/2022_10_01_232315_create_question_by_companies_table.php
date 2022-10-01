cd1618bf91151c23000852e4880368ada6be2432
Ali KARABAY <alikarabay77@gmail.com>
2022-10-02 02:32:58 +0300

    QuestionByCompany model and migration added.

---
+++ backend/database/migrations/2022_10_01_232315_create_question_by_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_by_companies', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->uuid('question_id')->index();
+            $table->uuid('company_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_by_companies');
+    }
+};
