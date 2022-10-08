9c880d6965c43dd47f4032ff08da72e91d8d992a
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-08 23:48:52 +0300

    Condition factory added.

---
+++ backend/database/factories/ConditionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Condition;
+use App\Models\QuestionCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Condition>
+ */
+class ConditionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'question_category_id' => QuestionCategory::factory(),
+            'condition_category_id' => fake()->date(),
+            'value' => fake()->randomNumber(),
+            'is_active' => Condition::STATUS_ACTIVE,
+        ];
+    }
+}
