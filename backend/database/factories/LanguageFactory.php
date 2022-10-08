a57a8901ce5afc5238c2c4fa2e78120f6a8afd5b
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-08 23:41:41 +0300

    Language factory added.

---
+++ backend/database/factories/LanguageFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Language;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Language>
+ */
+class LanguageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'code' => fake()->languageCode(),
+            'is_active' => Language::STATUS_ACTIVE,
+        ];
+    }
+}
