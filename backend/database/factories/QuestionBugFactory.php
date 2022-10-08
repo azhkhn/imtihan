5a4b8a74cbac2bb9a342f1bef69b8e607dd7f98b
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-09 00:27:22 +0300

    QuestionBug factory added.

---
+++ backend/database/factories/QuestionBugFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\QuestionBug>
+ */
+class QuestionBugFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'description' => fake()->text(),
+            'question_id' => Question::factory(),
+        ];
+    }
+}
