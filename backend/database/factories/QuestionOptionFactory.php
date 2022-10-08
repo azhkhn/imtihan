4cab9ce38a538173226ea7d736185127bc86bed0
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-09 00:32:03 +0300

    QuestionOption factory added.

---
+++ backend/database/factories/QuestionOptionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use App\Models\QuestionOption;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\QuestionOption>
+ */
+class QuestionOptionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'description' => fake()->text(),
+            'question_id' => Question::factory(),
+            'is_correct' => QuestionOption::STATUS_INACTIVE,
+            'image_path' => fake()->imageUrl(),
+        ];
+    }
+}
