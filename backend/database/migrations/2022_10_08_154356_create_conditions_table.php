6ac39810a1f9963cc8193dae6ab6637327acb0cd
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-08 18:54:14 +0300

    Conditions added

---
+++ backend/database/migrations/2022_10_08_154356_create_conditions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conditions', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('name');
+            $table->uuid('question_category_id')->index();
+            $table->uuid('condition_category_id')->index();
+            $table->float('value');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conditions');
+    }
+};
