34ef55fdeb57fd68db63e0af496621fe40090483
Ahmet Arşiv <ahmetarsivi@gmail.com>
2022-10-09 00:19:40 +0300

    PaymentMethod factory added.

---
+++ backend/database/factories/PaymentMethodFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PaymentMethod;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PaymentMethod>
+ */
+class PaymentMethodFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'code' => fake()->creditCardType(),
+            'description' => fake()->text(),
+            'is_active' => PaymentMethod::STATUS_ACTIVE,
+        ];
+    }
+}
