85e0b148be53ec239c666f805151edbe6827d21c
Ali KARABAY <alikarabay77@gmail.com>
2022-10-10 19:32:17 +0300

    Auhentication middleware added.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::apiResource('languages', LanguageController::class);
+    });
 });
-
-Route::apiResource('languages', LanguageController::class);
