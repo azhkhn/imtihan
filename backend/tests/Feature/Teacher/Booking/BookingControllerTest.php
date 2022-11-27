ca691fa9aa833836f321c373dd31c1fa8f96e8e8
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2022-11-27 03:23:05 +0300

    Bug fixed.

--- backend/tests/Feature/Teacher/Booking/BookingControllerTest.php
+++ backend/tests/Feature/Teacher/Booking/BookingControllerTest.php
@@ -19,11 +19,13 @@ class BookingControllerTest extends TestCase
     public function test_booking_list()
     {
         $company = Company::factory()->create();
-        $user = User::factory()->state(['role' => User::Teacher])->create();
-        UserInfo::factory()->state(['user_id' => $user->id, 'company_id' => $company->id])->create();
-        Booking::factory(20)->state(['company_id' => $company->id])->create();
+        $teacher = User::factory()->state(['role' => User::Teacher])->create();
+        UserInfo::factory()->state(['user_id' => $teacher->id, 'company_id' => $company->id])->create();
+        $student = User::factory()->state(['role' => User::Student])->create();
+        UserInfo::factory()->state(['user_id' => $student->id, 'company_id' => $company->id])->create();
+        Booking::factory(20)->state(['company_id' => $company->id, 'teacher_id' => $teacher->id, 'user_id' => $student->id])->create();
 
-        Sanctum::actingAs($user, ['teacher.booking.list']);
+        Sanctum::actingAs($teacher, ['teacher.booking.list']);
 
         $response = $this->get($this->apiUrl);
 
@@ -31,46 +33,20 @@ class BookingControllerTest extends TestCase
             ->assertJsonCount(20, 'data');
     }
 
-    public function test_booking_create()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->state(['role' => User::Teacher])->create();
-        UserInfo::factory()->state(['user_id' => $user->id, 'company_id' => $company->id])->create();
-        $booking = Booking::factory()->make();
-
-        Sanctum::actingAs($user, ['teacher.booking.create']);
-
-        $response = $this->postJson($this->apiUrl, $booking->toArray());
-        $response->assertStatus(201);
-    }
-
     public function test_booking_show()
     {
         $company = Company::factory()->create();
-        $user = User::factory()->state(['role' => User::Teacher])->create();
-        UserInfo::factory()->state(['user_id' => $user->id, 'company_id' => $company->id])->create();
-        $booking = Booking::factory()->state(['company_id' => $company->id])->create();
+        $teacher = User::factory()->state(['role' => User::Teacher])->create();
+        UserInfo::factory()->state(['user_id' => $teacher->id, 'company_id' => $company->id])->create();
+        $student = User::factory()->state(['role' => User::Student])->create();
+        UserInfo::factory()->state(['user_id' => $student->id, 'company_id' => $company->id])->create();
+        $booking = Booking::factory()->state(['company_id' => $company->id, 'teacher_id' => $teacher->id, 'user_id' => $student->id])->create();
 
-        Sanctum::actingAs($user, ['teacher.booking.show']);
+        Sanctum::actingAs($teacher, ['teacher.booking.show']);
 
         $response = $this->get($this->apiUrl.$booking->id);
         $response->assertJsonStructure(['success', 'message', 'data'])
             ->assertJsonFragment(['id' => $booking->id]);
-    }
-
-    public function test_booking_update()
-    {
-        $company = Company::factory()->create();
-        $user = User::factory()->state(['role' => User::Teacher])->create();
-        UserInfo::factory()->state(['user_id' => $user->id, 'company_id' => $company->id])->create();
-        $booking = Booking::factory()->state(['company_id' => $company->id])->create();
-
-        Sanctum::actingAs($user, ['teacher.booking.update']);
-
-        $response = $this->putJson($this->apiUrl.$booking->id, [
-            'description' => 'test',
-        ]);
-        $response->assertStatus(200);
     }
 
     public function test_booking_delete()
