0b6967c80c66385b93a7a456392476b3c841fddb
Ali KARABAY <alikarabay77@gmail.com>
2022-12-02 15:09:28 +0300

    Manager teacher list by company added

--- backend/app/Services/Manager/User/TeacherService.php
+++ backend/app/Services/Manager/User/TeacherService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Manager\User;
 
+use App\Helper\Helper;
 use App\Models\User;
 use App\Services\Base\BaseService;
 
@@ -10,5 +11,10 @@ class TeacherService extends BaseService
     public function __construct()
     {
         parent::__construct(User::class);
+    }
+
+    public function list(array $with = [], array $where = [])
+    {
+        return $this->model::with($with)->where($where)->whereRelation('info', 'company_id', Helper::userInfo()->company_id)->latest()->get();
     }
 }
